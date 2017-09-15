56dbd744a52f4c2986e68a3c81377e2c3acc8eea
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-09-15 14:48:36 -0600

    Using KMP variables

--- linux/benchmark.sh
+++ linux/benchmark.sh
@@ -20,10 +20,10 @@ function run_hybrid() {
 
   for size in ${sizes[@]}; do
     for rnk in ${ranks[@]}; do
-       export OMP_NUM_THREADS=$(( 64/rnk ))
-       export OMP_THREAD_LIMIT=$OMP_NUM_THREADS
-       export OMP_NESTED=TRUE
-       echo "Thilina: $OMP_NUM_THREADS"
+       export KMP_HOT_TEAMS_MODE=1
+       export KMP_HOT_TEAMS_MAX_LEVEL=2
+       export KMP_PLACE_THREADS = 1T
+       export KMP_AFFINITY=compact,granularity=fine
        mpirun -np $rnk n_body_simulation >> timings.${case}.hybrid.${size}
     done
   done
